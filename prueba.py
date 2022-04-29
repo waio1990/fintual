1a38426767513767ded320a4cc222d942a2c3ea3
waio1990 <ef.riveras@gmail.com>
2022-04-29 15:28:44 -0400

    Create prueba.py
    initial commit asdf asdf

---
+++ prueba.py
@@ -0,0 +1,29 @@
+#!Python
+from datetime import date
+
+class Portfolio(stocks):
+  # stocks es un diccionario con llave: Ticker de la Accion y valor: la cantidad de acciones
+  Stock=stocks
+  
+  def profit(self,Date1,Date2):
+  # entregar el profit entre 2 fechas.   
+    
+    inic=0
+    dolla=0
+    
+    # Dolares ganados (o perdidos)
+    for ticker,cantidad in self.Stock:
+      (diff)=(ticker.Price(Date2)-ticker.Price(Date1))*cantidad
+      dolla+=diff
+      
+    # Dolareces iniciales
+    for ticker,cantidad in self.Stock:
+      inicial=ticker.Price(Date1)*cantidad
+      inic+=inicial
+      
+    # return dolla/inic
+    # Anualizado
+      porc_an=(date(Date2)-date(Date1)).days/365
+      
+      return (dolla/inic)*inic
+  
